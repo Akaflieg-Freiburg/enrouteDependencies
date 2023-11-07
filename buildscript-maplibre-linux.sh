f64a88a05ff9add0250abe45db46bec74da9fb17
Stefan Kebekus <kebekus@users.noreply.github.com>
2023-11-07 08:05:05 +0100

    Cross-compile to Android arm64

--- buildscript-maplibre-linux.sh
+++ buildscript-maplibre-linux.sh
@@ -15,3 +15,18 @@ $Qt6_DIR_BASE/gcc_64/bin/qt-cmake \
     -DCMAKE_INSTALL_PREFIX=$Qt6_DIR_BASE/gcc_64
 cmake --build build-maplibre-native-qt-linux
 cmake --install build-maplibre-native-qt-linux
+
+echo
+echo "Android ARM64"
+mkdir -p build-maplibre-native-qt-android_arm64_v8a
+$Qt6_DIR_BASE/android_arm64_v8a/bin/qt-cmake \
+    -S  maplibre-native-qt \
+    -B build-maplibre-native-qt-android_arm64_v8a \
+    -G Ninja \
+    -DBUILD_TESTING=OFF \
+    -DCMAKE_C_COMPILER_LAUNCHER="ccache" \
+    -DCMAKE_CXX_COMPILER_LAUNCHER="ccache" \
+    -DCMAKE_PREFIX_PATH=$Qt6_DIR_BASE/android_arm64_v8a \
+    -DCMAKE_INSTALL_PREFIX=$Qt6_DIR_BASE/android_arm64_v8a
+cmake --build build-maplibre-native-qt-android_arm64_v8a
+cmake --install build-maplibre-native-qt-android_arm64_v8a
