4ee15faea485141322ccc059cdf9d288b9834152
Stefan Kebekus <stefan.kebekus@gmail.com>
2023-11-08 07:38:54 +0100

    Add more platforms

--- buildscript-maplibre-linux.sh
+++ buildscript-maplibre-linux.sh
@@ -32,3 +32,19 @@ $Qt6_DIR_BASE/android_arm64_v8a/bin/qt-cmake \
     -DCMAKE_INSTALL_PREFIX=$Qt6_DIR_BASE/android_arm64_v8a
 cmake --build build-maplibre-native-qt-android_arm64_v8a
 cmake --install build-maplibre-native-qt-android_arm64_v8a
+
+
+echo
+echo "Android X86_64"
+mkdir -p build-maplibre-native-qt-android_x86_64
+$Qt6_DIR_BASE/android_x86_64/bin/qt-cmake \
+    -S  maplibre-native-qt \
+    -B build-maplibre-native-qt-android_x86_64 \
+    -G Ninja \
+    -DBUILD_TESTING=OFF \
+    -DCMAKE_C_COMPILER_LAUNCHER="ccache" \
+    -DCMAKE_CXX_COMPILER_LAUNCHER="ccache" \
+    -DCMAKE_PREFIX_PATH=$Qt6_DIR_BASE/android_x86_64 \
+    -DCMAKE_INSTALL_PREFIX=$Qt6_DIR_BASE/android_x86_64
+cmake --build build-maplibre-native-qt-android_x86_64
+cmake --install build-maplibre-native-qt-android_x86_64
