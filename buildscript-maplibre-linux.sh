3f04127c442cf3aa7fae1915bb52b8aa8e1c71f3
Stefan Kebekus <kebekus@users.noreply.github.com>
2023-11-09 08:38:26 +0100

    More platforms

--- buildscript-maplibre-linux.sh
+++ buildscript-maplibre-linux.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export Qt6_DIR_BASE=/home/kebekus/Software/buildsystems/Qt/6.6.0
-
 echo
 echo "Linux Desktop"
 mkdir -p build-maplibre-native-qt-linux
@@ -19,6 +17,21 @@ cmake --build build-maplibre-native-qt-linux
 cmake --install build-maplibre-native-qt-linux
 
 echo
+echo "Android ARMv7"
+mkdir -p build-maplibre-native-qt-android_armv7
+$Qt6_DIR_BASE/android_armv7/bin/qt-cmake \
+    -S  maplibre-native-qt \
+    -B build-maplibre-native-qt-android_armv7 \
+    -G Ninja \
+    -DBUILD_TESTING=OFF \
+    -DCMAKE_C_COMPILER_LAUNCHER="ccache" \
+    -DCMAKE_CXX_COMPILER_LAUNCHER="ccache" \
+    -DCMAKE_PREFIX_PATH=$Qt6_DIR_BASE/android_armv7 \
+    -DCMAKE_INSTALL_PREFIX=$Qt6_DIR_BASE/android_armv7
+cmake --build build-maplibre-native-qt-android_armv7
+cmake --install build-maplibre-native-qt-android_armv7
+
+echo
 echo "Android ARM64"
 mkdir -p build-maplibre-native-qt-android_arm64_v8a
 $Qt6_DIR_BASE/android_arm64_v8a/bin/qt-cmake \
@@ -32,6 +45,22 @@ $Qt6_DIR_BASE/android_arm64_v8a/bin/qt-cmake \
     -DCMAKE_INSTALL_PREFIX=$Qt6_DIR_BASE/android_arm64_v8a
 cmake --build build-maplibre-native-qt-android_arm64_v8a
 cmake --install build-maplibre-native-qt-android_arm64_v8a
+
+
+echo
+echo "Android X86"
+mkdir -p build-maplibre-native-qt-android_x86
+$Qt6_DIR_BASE/android_x86/bin/qt-cmake \
+    -S  maplibre-native-qt \
+    -B build-maplibre-native-qt-android_x86 \
+    -G Ninja \
+    -DBUILD_TESTING=OFF \
+    -DCMAKE_C_COMPILER_LAUNCHER="ccache" \
+    -DCMAKE_CXX_COMPILER_LAUNCHER="ccache" \
+    -DCMAKE_PREFIX_PATH=$Qt6_DIR_BASE/android_x86 \
+    -DCMAKE_INSTALL_PREFIX=$Qt6_DIR_BASE/android_x86
+cmake --build build-maplibre-native-qt-android_x86
+cmake --install build-maplibre-native-qt-android_x86
 
 
 echo
