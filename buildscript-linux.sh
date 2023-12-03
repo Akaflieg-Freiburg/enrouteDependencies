024ccb51e9b952f4cb874d70dce9f00c60b07441
Stefan Kebekus <stefan.kebekus@math.uni-freiburg>
2023-12-03 08:02:43 +0100

    Play with actions

--- buildscript-linux.sh
+++ buildscript-linux.sh
@@ -3,6 +3,7 @@
 # Fail on first error
 set -e
 
+installDir=Qt/$(basename "$Qt6_DIR_BASE")/gcc_64
 
 echo
 echo "Linux Desktop"
@@ -16,6 +17,6 @@ $Qt6_DIR_BASE/gcc_64/bin/qt-cmake \
     -DCMAKE_C_COMPILER_LAUNCHER="ccache" \
     -DCMAKE_CXX_COMPILER_LAUNCHER="ccache" \
     -DCMAKE_PREFIX_PATH=$Qt6_DIR_BASE/gcc_64 \
-    -DCMAKE_INSTALL_PREFIX=$Qt6_DIR_BASE/gcc_64
+    -DCMAKE_INSTALL_PREFIX=$installDir
 cmake --build build-maplibre-native-qt-linux
 cmake --install build-maplibre-native-qt-linux
