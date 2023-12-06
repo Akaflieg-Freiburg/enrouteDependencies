6c9dca3d86b7fcb3888339dae9a3c286071e523f
Stefan Kebekus <stefan.kebekus@gmail.com>
2023-12-06 10:40:01 +0100

    Android

--- buildscript-android.sh
+++ buildscript-android.sh
@@ -6,8 +6,10 @@ set -e
 
 for PLATFORM in android_armv7 android_arm64_v8a android_x86 android_x86_64
 do
+	installDir=Qt/$(basename "$Qt6_DIR_BASE")/$PLATFORM
+
 	echo
-	echo "libzip - static for arm64 and x86_64 on iOS"
+	echo "libzip - static for $PLATFORM"
 
 	$Qt6_DIR_BASE/$PLATFORM/bin/qt-cmake \
     	-G Ninja  \
@@ -21,23 +23,23 @@ do
     	-DENABLE_BZIP2=OFF \
 	    -DENABLE_LZMA=OFF \
     	-DENABLE_ZSTD=OFF \
-    	-DCMAKE_INSTALL_PREFIX=$Qt6_DIR_BASE/$PLATFORM
+    	-DCMAKE_INSTALL_PREFIX=$installDir
 	cmake --build build-libzip-$PLATFORM
 	cmake --install build-libzip-$PLATFORM
 
 
     echo
-    echo "Android ARMv7"
+    echo "maplibre-native-qt - for $PLATFORMARM"
 
     $Qt6_DIR_BASE/$PLATFORM/bin/qt-cmake \
-	-S  maplibre-native-qt \
-	-B build-maplibre-native-qt-$PLATFORM \
-	-G Ninja \
-	-DBUILD_TESTING=OFF \
-	-DCMAKE_C_COMPILER_LAUNCHER="ccache" \
-	-DCMAKE_CXX_COMPILER_LAUNCHER="ccache" \
-	-DCMAKE_PREFIX_PATH=$Qt6_DIR_BASE/$PLATFORM \
-	-DCMAKE_INSTALL_PREFIX=$Qt6_DIR_BASE/$PLATFORM
+		-S maplibre-native-qt \
+		-B build-maplibre-native-qt-$PLATFORM \
+		-G Ninja \
+		-DBUILD_TESTING=OFF \
+		-DCMAKE_C_COMPILER_LAUNCHER="ccache" \
+		-DCMAKE_CXX_COMPILER_LAUNCHER="ccache" \
+		-DCMAKE_PREFIX_PATH=$Qt6_DIR_BASE/$PLATFORM \
+		-DCMAKE_INSTALL_PREFIX=$installDir
     cmake --build build-maplibre-native-qt-$PLATFORM
     cmake --install build-maplibre-native-qt-$PLATFORM
 done
